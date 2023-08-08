d824dc6f5adc99f7d5c68d759f02590712c48389
oleg59 <web-perm@bk.ru>
2023-08-08 17:58:02 +0700

    fix: add takeUntilDestroyed

--- projects/ng-custom-dashboard/src/lib/ng-custom-dashboard.component.ts
+++ projects/ng-custom-dashboard/src/lib/ng-custom-dashboard.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { GridsterConfig } from 'angular-gridster2';
 import { selectDashboard } from './state/dashboard/dashboard.reducer';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { selectGridsterConfig } from './state/config/config.reducer';
 import { DashboardItem } from './common/models/dashboard.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'ng-custom-dashboard',
@@ -15,12 +16,16 @@ import { DashboardItem } from './common/models/dashboard.model';
 export class NgCustomDashboardComponent implements OnInit {
   options$: Observable<GridsterConfig> = this.store.select(selectGridsterConfig);
   dashboard$ = new BehaviorSubject<DashboardItem[] | null>(null);
+  destroyRef = inject(DestroyRef);
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.store.select(selectDashboard).subscribe((v) => {
-      this.dashboard$.next(JSON.parse(JSON.stringify(v)) as DashboardItem[]);
-    });
+    this.store
+      .select(selectDashboard)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((v) => {
+        this.dashboard$.next(JSON.parse(JSON.stringify(v)) as DashboardItem[]);
+      });
   }
 }
